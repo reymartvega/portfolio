809209447997b68dfdca34457bc885ef0a531c43
reymartvega@gmail.com <reymartvega@gmail.com>
2023-02-27 13:13:56 +0800

    Changed environment variables

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ export const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.SERVICE_ID,process.env.TEMPLATE_ID, form.current,process.env.PUBLIC_KEY)
+    emailjs.sendForm(process.env.SERVICE_ID,process.env.TEMPLATE_ID, form.current,'vq1cTvxBZnZNmXp0b')
       .then((result) => {
           e.target.reset();
           Swal.fire({
